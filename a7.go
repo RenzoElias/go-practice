5290ea0076d2888fe82993abe6559f2a9aca3640
RenzoElias <renzoeliasdelacalle@gmail.com>
2022-10-03 11:58:20 -0500

    ADD func

--- a7.go
+++ a7.go
@@ -20,6 +20,8 @@ func main() {
 	
    fmt.Printf("Resultado de la suma %v\n, y el dato es de tipo %T\n", suma(9,13), suma(9,13))
 
+   // Si no te interesa un valor, se le pone el piso _
+   // value, _ := sumaDouble(9,13)
    value, valueAuxForType := sumaDouble(9,13)
    fmt.Printf("\nResultado de la suma %v\n, y el dato es de tipo %T\n\n", value, valueAuxForType)
 

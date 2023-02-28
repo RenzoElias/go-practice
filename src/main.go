bf2d7a98f2df4377b7b4c095adb65e5207da1009
RenzoElias <renzoeliasdelacalle@gmail.com>
2023-02-28 17:51:43 -0500

    ADD packages and punteros

--- src/main.go
+++ src/main.go
@@ -1,7 +1,69 @@
 package main
 
+import (
+	"fmt"
+
+	pk "github.com/RenzoElias/go-practice/src/mypackage"
+)
+
 // import "go-practice/src/mypackage"
 
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (namePC pc) ping() {
+	fmt.Println(namePC.brand, "Pong")
+}
+
+// *pc - Vamos acceder a los valores del struct mediante el puntero
+func (otherNamePC *pc) duplicateRAM() {
+	otherNamePC.ram = otherNamePC.ram * 2
+}
+
 func main() {
-	// var myCar mypackage.CarPublic´
+
+	var myCar pk.CarPublic
+	myCar.Brand = "Ferrari"
+	myCar.Year = 2020
+	fmt.Println(myCar)
+
+	var myOtherCar pk.CarPrivate
+	fmt.Println(myOtherCar)
+
+	pk.PrintMessage("Mundo")
+
+	// Privado por la primera palabra Minuscula
+	// Sea en Struct y tmb (tipos de datos dentro del struct)
+	// Funciones, Slice, Maps
+	// pk.printMessage("Mundo")
+
+	// PUNTEROS ===================================
+	a := 50
+	b := &a
+	// &a - acceso a la direccion de memoria (0x14000122020)
+	// *b - acceso al valor de la direccion de memoria (50)
+
+	fmt.Println(a, b, *b)
+
+	*b = 100
+	fmt.Println(a) // 100
+
+	myPC := pc{ram: 16, disk: 200, brand: "msi"}
+	fmt.Println(myPC)
+
+	// la funcion recibira el struct ya instanciado osea myPC
+	myPC.ping()
+
+	// myPC.ram = 32 // Mejor usar la funcion duplicateRAM
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+
+	fmt.Println(myPC)
+
 }
--- src/mypackage/mypackage.go
+++ src/mypackage/mypackage.go
@@ -1,7 +1,22 @@
 package mypackage
 
+import "fmt"
+
 // Car con acceso publico
 type CarPublic struct {
 	Brand string
 	Year  int
+}
+
+type CarPrivate struct {
+	brand string
+	year  int
+}
+
+func PrintMessage(s string) {
+	fmt.Println("Hola ", s)
+}
+
+func printMessage(s string) {
+	fmt.Println("Hola ", s)
 }
